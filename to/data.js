f09a1d56a5490a5ff4d430e78655c9e1e0e589b1
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-20 20:16:05 +0800

    Update data.js

--- to/data.js
+++ to/data.js
@@ -20,6 +20,9 @@ Description of ordinary method:
 Description of advanced method:
 [1]The function can customize the judgment according to the data in the url.
 [2]Return "break" can stop judging.
+====Water content====
+⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠                                   						
+
 ====The end====
 ****/
 //Start the ordinary method
@@ -38,6 +41,15 @@ urldata.push({name:"dingtalk",url:"https://www.dingtalk.com/",regexp:"i"})
 urldata.push({name:"helloworld",url:"data:text/html,hello world",regexp:"i"})
 urldata.push({name:"memes",url:"https://uu.163.com/m/message/12"})
 urldata.push({name:"new text",url:"data:text/text,"})
+urldata.push({name:"colorSelect",url:"/download/apps/smallapp/color-selected"})
+urldata.push({name:"czs",url:"/download/apps/smallapp/water-content"})
+urldata.push({name:"waterContent",url:"/download/apps/smallapp/water-content"})
+urldata.push({name:"yxh",url:"/download/apps/smallapp/reprint/yxhscq"})
+urldata.push({name:"atpeople",url:"/download/apps/smallapp/at-bilibili-user"})
+urldata.push({name:"4天瘦20斤完美计划",url:"/download/apps/smallapp/perfect-plan-for-4-days"})
+urldata.push({name:"perfect-plan-for-4-days",url:"/download/apps/smallapp/perfect-plan-for-4-days",regexp:"i"})
+urldata.push({name:"perfectPlanFor4Days",url:"/download/apps/smallapp/perfect-plan-for-4-days",regexp:"i"})
+urldata.push({name:"D4panS",url:"/download/apps/smallapp/perfect-plan-for-4-days"})
 
 //Stop the ordinary method and start the advanced method
 
