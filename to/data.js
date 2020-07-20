19d220e5744af686b4084394e839e48df77bf548
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-20 13:33:41 +0800

    Update data.js

--- to/data.js
+++ to/data.js
@@ -1,6 +1,28 @@
 (function(){
 window.urldata=[];
-//urldata.push({name:"",url:""})
+/****
+====Operation instruction====
+====Usage method====
+Ordinary method:
+urldata.push({name:"",url:""})
+Advanced method:
+urldata.push({function:function(){}})
+====Description====
+Method for passing in data(@data "The data you want to enter"):
+[1]https://shc7432.github.io/to?id=data
+[2]https://shc7432.github.io/to?to=data
+[3]https://shc7432.github.io/to?name=data
+[4]https://shc7432.github.io/to#data
+--------
+Description of ordinary method:
+@name "Url Data" 
+@url "Redirect to Url" 
+Description of advanced method:
+[1]The function can customize the judgment according to the data in the url.
+[2]Return "break" can stop judging.
+====The end====
+****/
+//Start the ordinary method
 urldata.push({name:"baidu",url:"https://www.baidu.com/s?q1=",regexp:"i"})
 urldata.push({name:"百度",url:"https://www.baidu.com/s?q1="})
 urldata.push({name:"bilibili",url:"https://www.bilibili.com/",regexp:"i"})
@@ -16,4 +38,8 @@ urldata.push({name:"dingtalk",url:"https://www.dingtalk.com/",regexp:"i"})
 urldata.push({name:"helloworld",url:"data:text/html,hello world",regexp:"i"})
 urldata.push({name:"memes",url:"https://uu.163.com/m/message/12"})
 urldata.push({name:"new text",url:"data:text/text,"})
+
+//Stop the ordinary method and start the advanced method
+
+//End
 })()
